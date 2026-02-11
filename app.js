8c28188f6c0127e2e20dbc5c0e47a8cd455de246
coronel2017pedro-gif <coronel2017pedro@gmail.com>
2026-02-11 14:55:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -809,7 +809,7 @@ async function bootstrap(){
   // SW register
   if("serviceWorker" in navigator){
     try{
-      await navigator.serviceWorker.register("./sw.js");
+      await navigator.serviceWorker.register("/colectas-pwa/sw.js");
     } catch (e) {
       // no bloquear operación
     }
@@ -881,3 +881,4 @@ document.addEventListener("DOMContentLoaded", async ()=>{
     }
   }, 1000);
 });
+
